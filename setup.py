c62f2333c7b1c49aec88b60e89ef004b2dd6a5df
Oncleben31 <oncleben31@users.noreply.github.com>
2018-10-05 13:51:25 +0200

    Try to fix pypi long description
    Linked to #11 
    Perhaps an encoding issue. If still not work we should check the tools versions required to deploy on pypi.org.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
